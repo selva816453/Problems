7941c32629f994f6d1c145f95524635a428a7ad2
selvam <selva816453@gmail.com>
2026-04-25 17:50:42 +0530

    Update Mirror Distance of an Integer.java

--- Mirror Distance of an Integer.java
+++ Mirror Distance of an Integer.java
@@ -1,4 +1,4 @@
-// Define its mirror distance as: abs(n - reverse(n))​​​​​​​ where reverse(n) is the integer formed by reversing the digits of n.
+ // Define its mirror distance as: abs(n - reverse(n))​​​​​​​ where reverse(n) is the integer formed by reversing the digits of n.
 // Return an integer denoting the mirror distance of n​​​​​​​.
 // abs(x) denotes the absolute value of x.
 
