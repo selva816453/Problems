eb8fb9d1219e0e1bb2376a4c6b5b2d162a439428
selvam <selva816453@gmail.com>
2026-05-06 18:26:27 +0530

    Power Of Numbers

---
+++ Power Of Numbers.java
@@ -0,0 +1,38 @@
+// Given a number n, find the value of n raised to the power of its own reverse.
+// Note: The result will always fit into a 32-bit signed integer.
+
+// Examples:
+
+// Input: n = 2
+// Output: 4
+// Explanation: The reverse of 2 is 2, and 22 = 4.
+
+// Input: n = 10
+// Output: 10
+// Explanation: The reverse of 10 is 1 (leading zero is discarded), and 10 raised to the power 1 is 10.
+
+
+// Constraints:
+// 1 ≤ n ≤ 10
+
+
+class Power Of Numbers{
+    public int reverseExponentiation(int n) {
+        // code here
+        int temp=n;
+        int rev=0;
+        while(n!=0){
+            int d=n%10;
+            rev=rev*10+d;
+            n/=10;
+        }
+        if(rev==1){
+            return temp;
+        }
+        int su=1;
+        for(int i=1;i<=temp;i++){
+            su*=temp;
+        }
+        return su;
+    }
+}
