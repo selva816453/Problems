7cee9aec9ed91db5be96df0f9077a44e74c13f71
selvam <selva816453@gmail.com>
2026-05-15 18:47:17 +0530

    Fix comment formatting in Common in 3 Sorted Arrays.java

--- Common in 3 Sorted Arrays.java
+++ Common in 3 Sorted Arrays.java
@@ -1,4 +1,4 @@
-// Given three sorted arrays in non-decreasing order, return all common elements in non-decreasing order across these arrays. If there are no such elements return an empty array.
+ // Given three sorted arrays in non-decreasing order, return all common elements in non-decreasing order across these arrays. If there are no such elements return an empty array.
 // Note: Ignore duplicates, include each common element only once in the output.
 
 // Examples :
